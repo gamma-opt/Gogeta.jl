882bff5dd3860b88d1935e0a9961aec02e80e80b
Joonatan Linkola <joonatan.linkola@gmail.com>
2023-05-21 20:01:08 +0300

    started working on BT procedures

--- src/JuMP_model.jl
+++ src/JuMP_model.jl
@@ -133,3 +133,104 @@ function evaluate(JuMP_model, input)
     end
     # println(JuMP_model)
 end
+
+
+# solves the optimal BT bounds
+function solve_optimal_bounds(nn_model, type = "predict", adversial_index = -1, opt_img_digit = 0)
+	@assert type == "predict" "Invalid type attribute \"$type\""
+	@assert 0 <= opt_img_digit <= 9 "Digit must be between 0 and 9."
+
+	K = length(nn_model) # NOTE! there are K+1 layers in the nn
+	nn_parameters = params(nn_model)
+	# println("nn_parameters: $nn_parameters")
+
+	W = [nn_parameters[2*i-1] for i in 1:K] # weights of the i:th layer are stored at the (2*i-1):th index
+	b = [nn_parameters[2*i] for i in 1:K] # biases  of the i:th layer are stored at the (2*i):th index
+	
+	# stores the node count of layer k (starting at layer k=0) at index k+1
+	input_node_count = length(nn_parameters[1][1,:])
+	node_count = [if k == 1 input_node_count else length(nn_parameters[2*(k-1)]) end for k in 1:K+1]
+
+	model = Model(Gurobi.Optimizer)
+	
+	# sets the variables x[k,j] and s[k,j], the relaxed z[k,j] and the big-M values U[k,j] and L[k,j]
+	@variable(model, x[k in 0:K, j in 1:node_count[k+1]] >= 0)
+	@variable(model, s[k in 1:K, j in 1:node_count[k+1]] >= 0)
+	@variable(model, 0 <= z[k in 1:K, j in 1:node_count[k+1]] <= 1)
+
+	# initial big-M values for L and U, input values fixed to 0 and 1 for MNIST
+	@variable(model, L[k in 0:K, j in 1:node_count[k+1]] == -1000)
+	@variable(model, U[k in 0:K, j in 1:node_count[k+1]] == 1000)
+	for j in 1:node_count[1]
+		fix(L[0,j], 0)
+		fix(U[0,j], 1)
+	end
+
+	for input_node in 1:node_count[1] # here k == 0
+		delete_lower_bound(x[0, input_node])
+		@constraint(model, L[0, input_node] <= x[0, input_node])
+		@constraint(model, x[0, input_node] <= U[0, input_node])
+	end
+
+	models = []
+
+	for k in 1:1#K
+
+		for node in 1:1#node_count[k+1]
+
+			if k <= K-1
+				temp_sum = sum(W[k][node,j] * x[k-1,j] for j in 1:node_count[k]) # NOTE! prev layer [k]
+				@constraint(model, const1, temp_sum + b[k][node] == x[k,node] - s[k,node])
+				@constraint(model, const2, x[k,node] <= U[k,node] * z[k,node])
+				@constraint(model, const3, s[k,node] <= -L[k,node] * (1 - z[k,node]))
+				# @variable(model, t_max == x[k,node] - s[k,node])
+				@objective(model, Max, x[k,node] - s[k,node])
+				
+
+
+			else # k = K
+
+			end
+		end
+
+	end
+
+	# # constraint (4b) and (4e) (cases k=1, ..., k=K)
+	# for k in 1:K
+	# 	for node in 1:node_count[k+1] # node count of the next layer of k, i.e., the layer k+1 
+	# 		if k == 1 # constraint (4b), case k=1
+	# 			temp_sum = sum(W[k][node,j] * x[k-1,j] for j in 1:node_count[k])
+	# 			@constraint(model, temp_sum + b[k][node] == x[k,node] - s[k,node])
+	# 		else
+	# 			# [j in node_count[k]] is the number of nodes of the layer k-1 (although indexed k)
+	# 			temp_sum = sum(W[k][node,j] * x[k-1,j] for j in 1:node_count[k])
+	# 			if k < K # constraint (4b), case k=2, ..., k=K-1
+	# 				@constraint(model, temp_sum + b[k][node] == x[k,node] - s[k,node])
+	# 			elseif k == K # constraint (4e) (k=K)
+	# 				@constraint(model, temp_sum + b[k][node] == x[k,node])
+	# 			end
+	# 		end
+	# 	end
+	# end
+
+	# # constraint (4d)
+	# @constraint(model, [k in 1:K, j in 1:node_count[k+1]], x[k,j] <= U[k,j] * z[k,j])
+	# @constraint(model, [k in 1:K, j in 1:node_count[k+1]], s[k,j] <= -L[k,j] * (1 - z[k,j]))
+
+	# # constraint (4f)
+	# for output_node in 1:node_count[K+1]
+	# 	delete_lower_bound(x[K, output_node])
+	# 	@constraint(model, L[K, output_node] <= x[K, output_node])
+	# 	@constraint(model, x[K, output_node] <= U[K, output_node])
+	# end
+
+	# if type == "predict"
+
+	# 	@objective(model, Max, x[K,opt_img_digit + 1]) # objective function for testing
+
+    # end
+
+	println(model)
+
+	return model
+end

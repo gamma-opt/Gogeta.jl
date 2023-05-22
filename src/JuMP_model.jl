1f138886fa115d7468eb9e0d44fc86077b40765d
Joonatan Linkola <joonatan.linkola@gmail.com>
2023-05-22 11:57:53 +0300

    commit to test pushing to github

--- src/JuMP_model.jl
+++ src/JuMP_model.jl
@@ -167,8 +167,8 @@ function solve_optimal_bounds(nn_model, input_U, input_L, type = "predict")
 
 			# sets the variables x[k,j] and s[k,j], the relaxed z[k,j] and the big-M values U[k,j] and L[k,j]
 			@variable(model, x[k in 0:K, j in 1:node_count[k+1]] >= 0)
-			@variable(model, s[k in 1:K, j in 1:node_count[k+1]] >= 0)
-			@variable(model, 0 <= z[k in 1:K, j in 1:node_count[k+1]] <= 1)
+			@variable(model, s[k in 1:K-1, j in 1:node_count[k+1]] >= 0)
+			@variable(model, 0 <= z[k in 1:K-1, j in 1:node_count[k+1]] <= 1)
 			@variable(model, U[k in 0:K, j in 1:node_count[k+1]] == 1000)
 			@variable(model, L[k in 0:K, j in 1:node_count[k+1]] == -1000)
 			# fix input bounds from input_U and input_L
@@ -177,10 +177,16 @@ function solve_optimal_bounds(nn_model, input_U, input_L, type = "predict")
 				fix(L[0,j], input_L[j])
 			end
 
-			for input_node in 1:node_count[1] # input constraints
+			for input_node in 1:node_count[1] # input constraints (4a)
 				delete_lower_bound(x[0, input_node])
 				@constraint(model, L[0, input_node] <= x[0, input_node])
 				@constraint(model, x[0, input_node] <= U[0, input_node])
+			end
+
+			for output_node in 1:node_count[K+1]
+				delete_lower_bound(x[K, output_node])
+				@constraint(model, L[K, output_node] <= x[K, output_node])
+				@constraint(model, x[K, output_node] <= U[K, output_node])
 			end
 
 			# NOTE! below constraints depending on the layer
@@ -188,27 +194,30 @@ function solve_optimal_bounds(nn_model, input_U, input_L, type = "predict")
 			for k_in in 0:k-1
 				if k_in >= 1
 					temp_sum = sum(W[k_in][node,j] * x[k_in-1,j] for j in 1:node_count[k_in]) # NOTE! prev layer [k_in]
-					@constraint(model, x[k_in,node] <= U[k_in,node] * z[k_in,node])
-					@constraint(model, s[k_in,node] <= -L[k_in,node] * (1 - z[k_in,node]))
+					@constraint(model, joo1,  x[k_in,node] <= U[k_in,node] * z[k_in,node])
+					@constraint(model, joo2, s[k_in,node] <= -L[k_in,node] * (1 - z[k_in,node]))
 
 					if k_in <= K-1-1
-						@constraint(model, temp_sum + b[k_in][node] == x[k_in,node] - s[k_in,node])
+						@constraint(model, joo3if, temp_sum + b[k_in][node] == x[k_in,node] - s[k_in,node])
 					else # k_in == K-1
-						@constraint(model, temp_sum + b[k_in][node] == x[k_in,node])
+						@constraint(model, joo3else, temp_sum + b[k_in][node] == x[k_in,node])
 					end
 				end
 			end
 
 			# NOTE! below constraints depending on the node
+			
 			temp_sum = sum(W[k][node,j] * x[k-1,j] for j in 1:node_count[k]) # NOTE! prev layer [k]
-			@constraint(model, x[k,node] <= U[k,node] * z[k,node])
-			@constraint(model, s[k,node] <= -L[k,node] * (1 - z[k,node]))
 
 			if k <= K-1
 				@constraint(model, temp_sum + b[k][node] == x[k,node] - s[k,node])
+				@constraint(model, x[k,node] <= U[k,node] * z[k,node])
+				@constraint(model, s[k,node] <= -L[k,node] * (1 - z[k,node]))
 				@objective(model, Max, x[k,node] - s[k,node])
 			else # k == K
 				@constraint(model, temp_sum + b[k][node] == x[k,node])
+				@constraint(model, L[k, node] <= x[k, node]) # const (4f) in layer K
+				@constraint(model, x[k, node] <= U[k, node])
 				@objective(model, Max, x[k,node])
 			end
 
@@ -217,40 +226,23 @@ function solve_optimal_bounds(nn_model, input_U, input_L, type = "predict")
 		end
 	end
 
-	# # constraint (4b) and (4e) (cases k=1, ..., k=K)
-	# for k in 1:K
-	# 	for node in 1:node_count[k+1] # node count of the next layer of k, i.e., the layer k+1 
-	# 		if k == 1 # constraint (4b), case k=1
-	# 			temp_sum = sum(W[k][node,j] * x[k-1,j] for j in 1:node_count[k])
-	# 			@constraint(model, temp_sum + b[k][node] == x[k,node] - s[k,node])
-	# 		else
-	# 			# [j in node_count[k]] is the number of nodes of the layer k-1 (although indexed k)
-	# 			temp_sum = sum(W[k][node,j] * x[k-1,j] for j in 1:node_count[k])
-	# 			if k < K # constraint (4b), case k=2, ..., k=K-1
-	# 				@constraint(model, temp_sum + b[k][node] == x[k,node] - s[k,node])
-	# 			elseif k == K # constraint (4e) (k=K)
-	# 				@constraint(model, temp_sum + b[k][node] == x[k,node])
-	# 			end
-	# 		end
-	# 	end
-	# end
-
-	# # constraint (4d)
-	# @constraint(model, [k in 1:K, j in 1:node_count[k+1]], x[k,j] <= U[k,j] * z[k,j])
-	# @constraint(model, [k in 1:K, j in 1:node_count[k+1]], s[k,j] <= -L[k,j] * (1 - z[k,j]))
-
-	# # constraint (4f)
-	# for output_node in 1:node_count[K+1]
-	# 	delete_lower_bound(x[K, output_node])
-	# 	@constraint(model, L[K, output_node] <= x[K, output_node])
-	# 	@constraint(model, x[K, output_node] <= U[K, output_node])
-	# end
-
-	# if type == "predict"
-
-	# 	@objective(model, Max, x[K,opt_img_digit + 1]) # objective function for testing
-
-    # end
-
 	return max_models
+end
+
+sum_U = [1,1]
+sum_L = [0,0]
+mdl = solve_optimal_bounds(sum_model, sum_U, sum_L)
+solve_bounds(mdl)
+
+function solve_bounds(opt_models)
+	opt_values = []
+
+	for model in opt_models
+		optimize!(model)
+		if has_values(model)
+			optimal = objective_value(model)
+			push!(opt_values, optimal)
+		else
+	end
+	return opt_values
 end

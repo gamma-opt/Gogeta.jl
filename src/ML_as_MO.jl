8f7df6333bb1708ccc4fc94a0aa41b7c69023022
Joonatan Linkola <joonatan.linkola@gmail.com>
2023-06-18 16:43:28 +0300

    minor fix

--- src/ML_as_MO.jl
+++ src/ML_as_MO.jl
@@ -3,4 +3,12 @@ module ML_as_MO
 include("JuMP_model.jl")
 include("bound_tightening.jl")
 
+export create_JuMP_model,
+    evaluate!
+
+export bound_tightening,
+    bound_tightening_threads
+    bound_tightening_workers
+    bound_calculating
+
 end # module

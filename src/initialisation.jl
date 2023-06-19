0ff66c8f6e76f5530d299f668c0c12b6aca0583c
Joonatan Linkola <joonatan.linkola@gmail.com>
2023-06-19 11:58:08 +0300

    new manifest and project

--- src/initialisation.jl
+++ src/initialisation.jl
@@ -10,23 +10,19 @@ using MLJBase # for conf matrix
 using Plots, Images
 using Statistics
 using Random
-using Serialization
 using Flux
 using Flux: params, train!, mse, flatten, onehotbatch
 using JuMP
 using JuMP: Model, value
-using HiGHS
 using Gurobi
 using EvoTrees
 using CSV
 using DataFrames
 using StatsBase
 using MLJ
-using JLD
 
 include("JuMP_model.jl")
 include("MNIST.jl")
-include("neural_nets.jl")
 include("bound_tightening.jl")
 
 include.(filter(contains(r".jl$"), readdir(current_dir*"/decision_trees/"; join=true)))

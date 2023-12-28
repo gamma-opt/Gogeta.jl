faafbe80814aed4bbb9a4f1f3b9e4c47d5b71d81
Nikita Belyak <nikita.belyak1994@gmail.com>
2023-12-28 16:20:12 +0200

    remove unnecessary information

--- docs/make.jl
+++ docs/make.jl
@@ -6,14 +6,7 @@ DocMeta.setdocmeta!(Gogeta, :DocTestSetup, :(using Gogeta); recursive=true)
 makedocs(;
     modules=[Gogeta],
     authors="Nikita Belyak <nikita.belyak1994@gmail.com> and contributors",
-    repo="https://github.com/'Nikita Belyak'/Gogeta.jl/blob/{commit}{path}#{line}",
     sitename="Gogeta.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://'Nikita Belyak'.github.io/Gogeta.jl",
-        edit_link="main",
-        assets=String[],
-    ),
     pages=[
         "Home" => "index.md",
         "Reference" => "reference.md",

5dad35b906d6014dc32da54596afca0f78d256c6
Eetu Reijonen <eetujj.reijonen@gmail.com>
2024-01-15 14:38:09 +0200

    fixed file imports

--- src/Gogeta.jl
+++ src/Gogeta.jl
@@ -2,27 +2,25 @@ module Gogeta
 
 #include the files for neural networks functions and export the functions
 
-include("nn/JuMP_model.jl")
+include("neural_networks/JuMP_model.jl")
 export create_JuMP_model, evaluate!
 
-include("nn/CNN_JuMP_model.jl")
+include("neural_networks/CNN_JuMP_model.jl")
 export create_CNN_JuMP_model, evaluate_CNN!
 
-include("nn/bound_tightening.jl")
+include("neural_networks/bound_tightening.jl")
 export bound_tightening,
     bound_tightening_threads,
     bound_tightening_workers,
     bound_tightening_2workers
 
-#include the files for decision trees functions and export the functions
+#include the files for tree tree_ensembles functions and export the functions
 
-include("dt/tree_model_to_MIP.jl")
+include("tree_ensembles/tree_model_to_MIP.jl")
 export tree_model_to_MIP
 
-include("dt/types.jl")
+include("tree_ensembles/types.jl")
 export TEModel,
     extract_evotrees_info
-
-# Write your package code here.
 
 end

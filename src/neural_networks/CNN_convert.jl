2aff7fc0b598e04ed64bcb9c1c97f35a7034fa61
Eetu Reijonen <eetujj.reijonen@gmail.com>
2024-02-23 17:41:32 +0200

    flatten layer to input layer completed

--- src/neural_networks/CNN_convert.jl
+++ src/neural_networks/CNN_convert.jl
@@ -6,6 +6,7 @@ function create_model(CNN_model)
     @variable(jump_model, c[layer=0:2, x=1:sizes(layer), y=1:sizes(layer), channel=1:channels(layer)] >= 0)
     @variable(jump_model, cs[layer=1:2, x=1:sizes(layer), y=1:sizes(layer), channel=1:channels(layer)] >= 0)
     @variable(jump_model, cz[layer=1:2, x=1:sizes(layer), y=1:sizes(layer), channel=1:channels(layer)], Bin)
+    @variable(jump_model, x[layer=0:1, if layer == 0 neuron=1:2560 else neuron=1:10 end])
 
     for (index, layer) in enumerate(CNN_model)
         if layer isa Conv
@@ -48,16 +49,23 @@ function create_model(CNN_model)
             for x in 1:sizes(index), y in 1:sizes(index)
                 # TODO include previous filter in formulas
                 # TODO include pad and stride
-                for image in channels(index-1)
-                    pooling = @expression(jump_model, sum([filters[filter][filter_size+1-i, filter_size+1-j] * c[index-1, x-filter_border+i, y-filter_border+j, 1] for i in 1:filter_size, j in 1:filter_size]))
+                for image in 1:channels(index-1)
                     
-                    # @constraint(jump_model, c[index, x, y, image] >= pooling)
+                    @constraint(jump_model, [i in -1:1, j in -1:1], c[index, x, y, image] >= c[index-1, x*filter_size - filter_border + i, y*filter_size - filter_border + j, image])
+                    
+                    z = @variable(jump_model, [-1:1, -1:1], Bin)
+                    @constraint(jump_model, sum([z[i, j] for i in -1:1, j in -1:1]) == 1)
+
+                    @constraint(jump_model, [i in -1:1, j in -1:1], z[i, j] => {c[index, x, y, image] <= c[index-1, x*filter_size - filter_border + i, y*filter_size - filter_border + j, image]})
                 end
             end
         elseif layer == Flux.flatten
             println("Layer $index is flatten")
             println("Size of input: $(size(CNN_model[1:index-1](input)))")
             println("Size of output: $(size(CNN_model[1:index](input)))")
+
+            @constraint(jump_model, [channel in 1:10, width in 1:16, height in 1:16], x[0, width + (height-1)*16 + (channel-1)*256] == c[index-1, width, height, channel])
+
         elseif layer isa Dense
             println("Layer $index is dense")
             println("Weights: $(size(Flux.params(layer)[1]))")
@@ -69,9 +77,11 @@ function create_model(CNN_model)
     return jump_model
 end
 
-function image_pass!(jump_model, input, index)
+function image_pass!(jump_model, input, index, layer)
     [fix(jump_model[:c][0, x, y, 1], input[x, y, 1, 1], force=true) for x in 1:50, y in 1:50]
-    @objective(jump_model, Min, jump_model[:c][1, 1, 1, 1])
     optimize!(jump_model)
-    return [value(jump_model[:c][1, x, y, index]) for x in 1:48, y in 1:48]
+    if layer == 1 return [value(jump_model[:c][1, x, y, index]) for x in 1:48, y in 1:48]
+    elseif layer == 2 return [value(jump_model[:c][2, x, y, index]) for x in 1:16, y in 1:16]
+    elseif layer == 3 return [value(jump_model[:x][0, n]) for n in 1:2560]
+    end
 end
--- src/neural_networks/CNN_to_MIP.jl
+++ src/neural_networks/CNN_to_MIP.jl
@@ -20,19 +20,29 @@ downscaled_image = imresize(image, (50, 50));
 
 input = reshape(Float32.(channelview(Gray.(downscaled_image))), 50, 50, 1, 1);
 input = input[end:-1:1, :, :, :];
+
+# input image
 heatmap(input[:, :, 1, 1], background=false, legend=false, color=:inferno, aspect_ratio=:equal, axis=([], false))
 
+# convolution layer outputs
 outputs = [CNN_model[1](input)[:, :, i, 1] for i in 1:size(CNN_model[1:2](input))[3]];
 display.(heatmap.(outputs, background=false, legend=false, color = :inferno, aspect_ratio=:equal, axis=([], false)));
 
-jump = create_model(CNN_model)
-out = image_pass!(jump, input, 3);
-
-CNN_model[1](input)[:, :, 1, 1] ≈ out
-
-heatmap(CNN_model[1](input)[:, :, 3, 1], background=false, legend=false, color=:inferno, aspect_ratio=:equal, axis=([], false))
-heatmap(out, background=false, legend=false, color=:inferno, aspect_ratio=:equal, axis=([], false))
-
+# maxpool outputs
 outputs = [CNN_model[1:2](input)[:, :, i, 1] for i in 1:size(CNN_model[1:2](input))[3]];
 display.(heatmap.(outputs, background=false, legend=false, color = :inferno, aspect_ratio=:equal, axis=([], false)));
+
+# create jump model from cnn
+jump = create_model(CNN_model)
+
+# Test that jump model produces same outputs for all layers as the CNN
+all([CNN_model[1](input)[:, :, i, 1] ≈ image_pass!(jump, input, i, 1) for i in 1:10])
+all([CNN_model[1:2](input)[:, :, i, 1] ≈ image_pass!(jump, input, i, 2) for i in 1:10])
+vec(CNN_model[1:3](input)) ≈ image_pass!(jump, input, 0, 3)
+
+# Plot true model maxpool fifth channel
+heatmap(CNN_model[1:2](input)[:, :, 5, 1], background=false, legend=false, color=:inferno, aspect_ratio=:equal, axis=([], false))
+
+# Plot jump model maxpool fifth channel
+heatmap(image_pass!(jump, input, 5, 2), background=false, legend=false, color=:inferno, aspect_ratio=:equal, axis=([], false))
 

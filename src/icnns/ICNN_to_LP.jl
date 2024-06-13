71de7629fea941bf1ed0934c267380405d9e4cb0
Eetu Reijonen <eetujj.reijonen@gmail.com>
2024-06-13 15:50:48 +0300

    improvements to ICNN to LP formulation - anonymous variables

--- src/icnns/ICNN_to_LP.jl
+++ src/icnns/ICNN_to_LP.jl
@@ -1,5 +1,5 @@
 """
-    function ICNN_formulate!(jump::JuMP.Model, filepath::String)
+    function ICNN_formulate!(jump::JuMP.Model, filepath::String, output_var, input_vars...)
 
 Formulates the input convex neural network (ICNN) as an LP into a JuMP model. The model parameters must be contained in a JSON file located in the given filepath. For more information about ICNNs see Amos et al. (2017).
 
@@ -9,15 +9,19 @@ JSON file structure:
 - 'SKIP' indicates a skip connection layer. These only contain weights.
 - For a clear example on how to export these parameters from Tensorflow, see examples/-folder of the package repository
 
-This function modifes the JuMP model given as input and outputs variables references to the ICNN input and output variables in the JuMP model. Also modifies the JuMP model objective function to include the ICNN output as a penalty term.
+This function modifies the JuMP model given as input. The input and output variable references that are given as input to this function will be linked to the appropriate variables in the ICNN formulation. No additional names are added to the JuMP model - all variables are added as anonymous. Also the JuMP model objective function is modified to include the ICNN output as a penalty term.
 
 # Arguments
 
 - `jump`: JuMP model where the LP formulation should be saved
 - `filepath`: relative path to the JSON file containing the model parameters
+- `output_var`: reference to the variable that should be linked to the ICNN output
+- `input_vars`: references to the variables that will be used as the ICNN inputs
 
 """
-function ICNN_formulate!(jump::JuMP.Model, filepath::String)
+function ICNN_formulate!(jump::JuMP.Model, filepath::String, output_var, input_vars...)
+
+    @assert objective_sense(jump) in [MAX_SENSE, MIN_SENSE] "Objective sense (Min/Max) must be set."
 
     W = JSON.parsefile(filepath)
     # Weights: W[layer name][1][column num][row]
@@ -35,8 +39,11 @@ function ICNN_formulate!(jump::JuMP.Model, filepath::String)
         n_neurons[layer] = length(W["FC"*string(layer)][2])
     end
 
-    @variable(jump, z[layer=union(0, layers), 1:n_neurons[layer]])
+    z = @variable(jump, [layer=union(0, layers), 1:n_neurons[layer]])
     @constraint(jump, [layer=layers[1:end-1], neuron=1:n_neurons[layer]], z[layer, neuron] >= 0)
+
+    input = [var for var in input_vars]
+    @constraint(jump, [neuron=1:n_neurons[0]], z[0, neuron] == input[neuron])
 
     for layer in layers
         for neuron in 1:n_neurons[layer]
@@ -53,27 +60,36 @@ function ICNN_formulate!(jump::JuMP.Model, filepath::String)
         end
     end
 
-    # reference to input and output variables
-    input_var = jump[:z][0, :]
-    output_var = jump[:z][maximum(layers), 1]
+    # reference to output variable
+    @constraint(jump, output_var == z[maximum(layers), 1])
 
-    @assert objective_sense(jump) != MAX_SENSE "Objective must be minimization with convex functions"
+    sign = objective_sense(jump) == JuMP.MAX_SENSE ? -1.0 : 1.0
 
     # modify objective to include penalty term
-    @objective(jump, Min, objective_function(jump) + output_var)
-
-    return input_var, output_var
+    @objective(jump, objective_sense(jump), objective_function(jump) + sign * output_var)
 end
 
-function forward_pass_ICNN!(jump, input)
-    @assert length(input) == length(jump[:z][0, :]) "Incorrect input length."
-    [fix(jump[:z][0, neuron], input[neuron]) for neuron in eachindex(input)]
+"""
+    function forward_pass_ICNN!(jump, input, output_var, input_vars)
+
+Calculates the forward pass through the ICNN (the output that is produced with the given input). 
+
+# Arguments
+
+- `jump`: JuMP model with the ICNN
+- `input`: value to be forwaard passed
+- `output_var`: reference to the ICNN output variable
+- `input_vars`: references to the ICNN input variables
+
+"""
+function forward_pass_ICNN!(jump, input, output_var, input_vars)
+
+    input_var = [var for var in input_vars]
+    [fix(input_var[neuron], input[neuron]) for neuron in eachindex(input)]
     
     try
         optimize!(jump)
-        (last_layer, _) = maximum(keys(jump[:z].data))
-        result = value.(jump[:z][last_layer, 1])
-        return result
+        return value(output_var)
     catch e
         println("Error with forward pass: $e")
         @warn "Input or output outside of bounds or incorrectly constructed model."
